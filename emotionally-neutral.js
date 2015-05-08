b5070c68d87f3690b4a9d62ccbf2b61ea2b84003
josegallegos07 <jose.gallegos@brainplasticity.com>
2015-05-07 21:39:26 -0700

    include rgb

--- emotionally-neutral.js
+++ emotionally-neutral.js
@@ -8,28 +8,36 @@
     _app.colors = [
       {
         "name": "white",
-        "hex": "#FFFFFF"
+        "hex": "#FFFFFF",
+        "rgb": "(255,255,255)"
       }, {
         "name": "grey",
-        "hex": "#BEBEBE"
+        "hex": "#BEBEBE",
+        "rgb": "(190,190,190)"
       }, {
         "name": "black",
-        "hex": "#000000"
+        "hex": "#000000",
+        "rgb": "(0,0,0)"
       }, {
         "name": "red",
-        "hex": "#FF0000"
+        "hex": "#FF0000",
+        "rgb": "(255,0,0)"
       }, {
         "name": "blue",
-        "hex": "#0000FF"
+        "hex": "#0000FF",
+        "rgb": "(0,0,255)"
       }, {
         "name": "yellow",
-        "hex": "#FFFF00"
+        "hex": "#FFFF00",
+        "rgb": "(255,255,0)"
       }, {
         "name": "green",
-        "hex": "#008000"
+        "hex": "#008000",
+        "rgb": "(0,128,0)"
       }, {
         "name": "purple",
-        "hex": "#800080"
+        "hex": "#800080",
+        "rgb": "(128,0,128)"
       }
     ];
     template = $('body').html();
